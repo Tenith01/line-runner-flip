4752b3a31e4d28f449b7a43f9a15301802d614bc
Tenith Hasintha <hasintha199@gmail.com>
2022-09-19 18:31:30 +0530

    Initialize Mein menu system

--- Assets/Scripts/E-View/PlayerView.cs
+++ Assets/Scripts/E-View/PlayerView.cs
@@ -1,26 +1,40 @@
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using Zenject;
 
 public class PlayerView : MonoBehaviour
 {
+    [Inject] private ISessionPresenter _sessionPresenter;
     [Inject] private IPlayerPresenter playerPresenter;
 
     private void Start()
     {
         playerPresenter.moveInputStream.Subscribe(RotatePlayer);
+        this.OnTriggerEnter2DAsObservable().Subscribe(collider => AddScore(collider)).AddTo(this);
+        ;
     }
 
     private void RotatePlayer(float rotateValue)
     {
         if (rotateValue > 0)
         {
-            transform.position =new Vector3(-4.04f, 0.8f, 0f);
-
+            transform.position = new Vector3(-4.04f, 0.8f, 0f);
         }
         else
         {
-            transform.position =new Vector3(-4.04f,-0.8f,0f);
+            transform.position = new Vector3(-4.04f, -0.8f, 0f);
+        }
+    }
+
+    private void AddScore(Collider2D collider2D)
+    {
+        if (collider2D.gameObject.CompareTag("AddScore"))
+        {
+            _sessionPresenter.AddScore(1);
+        }
+        else if (collider2D.gameObject.CompareTag("Enemy"))
+        {
         }
     }
 }
---
+++ Assets/Scripts/E-View/UI Canvas/InGameUIView.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class InGameUIView : MonoBehaviour
+{
+    [SerializeField] 
+    private TextMeshProUGUI scoreText;
+    
+    [Inject] private ISessionPresenter _sessionPresenter;
+
+    [SerializeField] 
+    private GameObject MainMenu;
+    private void Start()
+    {
+        _sessionPresenter.scoreRx.Subscribe(score => scoreText.text = "Score : " + score.ToString()).AddTo(this);
+    }
+
+    public void ShowMenMenu()
+    {
+        gameObject.SetActive(false);
+        MainMenu.SetActive(true);
+        
+    }
+}
